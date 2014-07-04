6f9a4bfd0fa15ebdbfdfffc3237664e51196f6df
Magne Westlie <magne@appresso.no>
2014-07-04 13:05:34 +0200

    Added simpler file for demo purposes.

--- README.md
+++ README.md
@@ -5,7 +5,9 @@ A library for finding similarities in code.
 
 ## Motivation
 
-Created to find similarities of a set of code files. 
+Created to find similarities of a set of code files. Used to compare a set of
+hand-ins in programming education courses, and can report if similarity of code
+in files are high.
 
 ## Usage
 
@@ -14,7 +16,11 @@ and operators for a spesific language.
 
 ## Install
 
-This package will be added to pypi soon. No virtualenv? Currently this package uses standard Python modules only.
+This package will be added to pypi soon, but for now:
+
+    $ git clone git@github.com:apparator/detektor.git
+
+No virtualenv? Currently this package uses standard Python modules only.
 
 ## Run demo
 
--- demo1.py
+++ demo1.py
@@ -1,6 +1,18 @@
 """ Get detektor signature for code in a given file.
 
-Opens the file 'demo_files/donny/find.py' and sends this to the detektor codeparser.
+Opens the file 'demo_files/donny/helloworldplus.py' and sends this to the
+detektor codeparser.
+
+The file looks like this:
+
+    import os
+
+    def add(a, b):
+        return a + b
+
+    print 'Hello %s!' % 'world'
+    a = 1
+    print '%s + %s equals %s' % (a, a, add(a, a))
 
 Prints misc info.
 """
@@ -13,9 +25,9 @@ import config
 from libs.defgetter import defgetter
 from libs.codeparser import Parser
 
-filepath = os.path.abspath('demo_files/donny/find.py')
+filepath = os.path.abspath('demo_files/donny/helloworldplus.py')
 
-print 'Open file "demo_files/donny/find.py"'
+print 'Open file "demo_files/donny/helloworldplus.py"'
 filehandler = open(filepath, 'r')
 
 print 'Send filehandler to codeparser'
