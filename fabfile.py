4f343c5c312502b2c76d25593c6b09a212f98d56
Magne Westlie <magne@appresso.no>
2014-07-04 12:12:58 +0200

    Added demo command to fab.

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from fabric.api import run
+from fabric.api import run, local
 
 DETEKTOR = os.path.dirname(__file__)
 os.environ.update({'DETEKTOR': DETEKTOR})
@@ -11,3 +11,11 @@ def test():
     import unittest
     testsuite = unittest.TestLoader().discover('test')
     unittest.TextTestRunner(verbosity=1).run(testsuite)
+
+def demo(num=0):
+    num = int(num)
+    if not num in (1,2):
+        print 'Choose demo 1 or 2. Run with e.g "fab demo:1"'
+        return
+    local('python demo{}.py'.format(num))
+    
