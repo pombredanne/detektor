c6fd59466c98c21a33f061e9d2fbce9af535b619
Magne Westlie <magne@appresso.no>
2014-08-03 22:07:59 +0200

    Added test for signature method that takes object or list of objects.

--- detektor.py
+++ detektor.py
@@ -21,7 +21,7 @@ def set_detektor_signature_on_single_object(obj, filepath_attribute):
 
     Arguments:
     obj -- The obj that the detektor_signature attribute should be added to.
-    filepath_attribute -- The attribute that holds the attribute path.
+    filepath_attribute -- The attribute that holds the file path of the code.
 
     Examples:
     set_detektor_signature_on_single_object(assignment, 'fileobj.path')
@@ -34,3 +34,32 @@ def set_detektor_signature_on_single_object(obj, filepath_attribute):
     s = p.get_code_signature()
     obj.detektor_signature = DetektorSignature(**s)
     return obj
+
+
+def set_detektor_signature_on_list_of_objects(object_list, filepath_attribute):
+    """Set the "detektor_signature" on a list of objects.
+
+    Arguments:
+    object_list -- The list of objects that the detektor_signature attribute should be added to.
+    filepath_attribute -- The attribute that holds the file path of the code.
+
+    Examples:
+    set_detektor_signature_on_list_of_objects(assignments, 'fileobj.path')
+    """
+    return [set_detektor_signature_on_single_object(o, filepath_attribute) for o in object_list]
+
+
+def set_detektor_signature(obj, filepath_attribute):
+    """Set the "detektor_signature" on list of, or object.
+
+    Arguments:
+    obj -- The obj that the detektor_signature attribute should be added to. Assumed to be iterable.
+    filepath_attribute -- The attribute that holds the attribute path.
+
+    Examples:
+    set_detektor_signature_on_single_object(assignment, 'fileobj.path')
+    """
+    try:
+        return set_detektor_signature_on_list_of_objects(obj, filepath_attribute)
+    except:
+        return set_detektor_signature_on_single_object(obj, filepath_attribute)
--- test/test_set_code_signature_attribute_on_list_of_objects.py
+++ test/test_set_code_signature_attribute_on_list_of_objects.py
@@ -12,25 +12,31 @@ class MockAssignment(object):
         self.f = MockFileClass(filepath)
 
 
-class TestSetSignatureOnObject(unittest.TestCase):
+class TestSetSignatureOnListOfObjects(unittest.TestCase):
     def setUp(self):
         filepath = 'test/pyfiles/helloworldplus.py'
         self.mockassignment = MockAssignment(filepath)
+        self.assignments = [
+            self.mockassignment,
+            self.mockassignment,
+        ]
 
     def tearDown(self):
         del self.mockassignment
 
-    def test_detektor_signature_var_set_on_object(self):
-        self.mockassignment = detektor.set_detektor_signature_on_single_object(self.mockassignment, 'f.path')
-        self.assertTrue(hasattr(self.mockassignment, 'detektor_signature'))
+    def test_detektor_signature_var_set_on_list_of_objects(self):
+        alist = detektor.set_detektor_signature_on_list_of_objects(self.assignments, 'f.path')
+        for a in alist:
+            self.assertTrue(hasattr(a, 'detektor_signature'))
 
-    def test_detektor_signature_set_raises_exception_on_non_existing_deep_path(self):
+    def test_detektor_signature_set_raises_exception_on_wrong_file_path(self):
         with self.assertRaises(AttributeError):
-            detektor.set_detektor_signature_on_single_object(
-                self.mockassignment,
+            detektor.set_detektor_signature_on_list_of_objects(
+                self.assignments,
                 'f.non_existing_path')
 
     def test_detektor_signature_on_object_raises_on_non_existing_file(self):
         mockass = MockAssignment('does/not/exist.py')
+        assignments = [mockass, mockass]
         with self.assertRaises(IOError):
-            mockass = detektor.set_detektor_signature_on_single_object(mockass, 'f.path')
+            mockass = detektor.set_detektor_signature_on_list_of_objects(assignments, 'f.path')
