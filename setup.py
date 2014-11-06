14eb799b9e8148400c6a08120fa9f361aa46ab64
Espen A. Kristiansen <post@espenak.net>
2014-11-07 00:54:54 +0100

    Released 1.1.0-beta.003.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='detektor',
     description='A library for finding similarities in code.',
-    version='1.1.0-beta.002',
+    version='1.1.0-beta.003',
     url='https://github.com/appressoas/detektor',
     author='Magne Westlie, Espen Angell Kristiansen, Tor Ivar Johansen',
     author_email='magne@appresso.no,espen@appresso.no,tor@appresso.no',
